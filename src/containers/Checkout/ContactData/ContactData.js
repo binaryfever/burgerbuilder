6c0ad99c2289b28d04003a980f726fa7d576fe53
Fred McHale <binaryfever@gmail.com>
2020-05-29 17:45:40 -0700

    Added the contact form via formik)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage} from 'formik';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
@@ -6,48 +7,79 @@ import * as FirestoreService from '../../../services/firestore';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 
 const ContactData = (props) => {
-  const [user, setUser] = useState({ name: '', email: '', address: {
-    street: '',
-    postalCode: ''
-  }});
   const [loading, setLoading] = useState(false);
-
-  const orderHandler = async (event) => {
-    console.log(props); 
-    event.preventDefault(); 
-      setLoading(true); 
-      const order = {
-        ingredients: props.ingredients,
-        price: props.totalPrice,
-        customer: {
-          name: "Fred McHale",
-          address: {
-            street: "Test Street",
-            zipcode: 1234,
-            country: "USA"
-          },
-          email: "test@test.com"
-        },
-        deliverMethod: "fastest"
-      }
-    
-    try{
-      await FirestoreService.createOrder(order);
-      setLoading(false);
-      props.history.push('/');
-    }catch(error){
-      setLoading(false);
-    }
-  };
   
   let form = (
-      <form>
-        <input type="text" name="name" placeholder="Your Name" />
-        <input type="email" name="email" placeholder="Your Email" />
-        <input type="text" name="street" placeholder="Your Street" />
-        <input type="text" name="postalCode" placeholder="Your Postal Code" />
-        <Button btnType="Success" clicked={orderHandler}>Order</Button>
-      </form>
+      <Formik
+        initialValues={{name: '', email:'', street:'', postalCode:'', deliveryMethod:'fastest'}}
+          validate={values => {
+          const errors = {};
+          if(!values.name){
+            errors.name = 'Name required';
+          }
+
+          if(!values.email){
+            errors.email = 'Email required';
+          }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
+            errors.email = 'Invalid email address';
+          }
+
+          if(!values.street){
+            errors.street = 'Street Required';
+          }
+
+          if(!values.postalCode){
+            errors.postalCode = 'Postal Code Required';
+          }
+
+          return errors;
+        }}
+        onSubmit={async (values, {setSubmitting}) => {
+          setLoading(true); 
+          const order = {
+            ingredients: props.ingredients,
+            price: props.totalPrice,
+            customer: {
+              name: values.name,
+              address: {
+                street: values.street,
+                zipCode: values.postalCode,
+                country: "USA"
+              },
+              email: values.email
+            },
+            deliveryMethod: values.deliveryMethod
+          }
+          
+          try{
+            await FirestoreService.createOrder(order);
+            setLoading(false);
+            props.history.push('/');
+          }catch(error){
+            setLoading(false);
+          }
+        }}
+      >
+        {({isSubmitting}) => (
+          <Form>
+            <Field type="text" name="name" placeholder="Name"/>
+            <ErrorMessage name="name" component="div" className={classes.Invalid}/>
+            <Field type="email" name="email" placeholder="Email"/>
+            <ErrorMessage name="email" component="div" className={classes.Invalid}/>
+            <Field type="text" name="street" placeholder="Your Street"/>
+            <ErrorMessage name="street" component="div" className={classes.Invalid}/>
+            <Field type="text" name="postalCode" placeholder="Postal Code"/>
+            <ErrorMessage name="postalCode" component="div" className={classes.Invalid}/>
+            <Field as="select" name="deliveryMethod">
+              <option value="fastest">Fastest</option>
+              <option value="cheapest">Cheapest</option>
+            </Field>
+            <Button type="submit" btnType="Success" disabled={isSubmitting}>
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
   );
 
   if(loading){
