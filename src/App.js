cc48bc1f1756d2b95a7f6c511f268d9c53ef0444
Fred McHale <binaryfever@gmail.com>
2020-06-24 16:44:56 -0700

    Guarded routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,19 +8,50 @@ import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Login from './containers/Auth/Login/Login';
 import Logout from './containers/Auth/Logout/Logout';
+import FireauthService from './services/firebase';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false); 
+  
+  useEffect( () => {
+    const unregisterObserver = FireauthService.auth.onAuthStateChanged(
+      (user) => {
+        if(user){
+          setAuthenticated(!!user);
+        }
+      }
+    );
+    return function cleanUp(){
+      unregisterObserver();
+    };
+  },[]);
+
+  //Guard the routes
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/login" component={Login} />
+      <Route path="/" component={BurgerBuilder} /> 
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if(authenticated){
+    routes = (
+    <Switch>
+      <Route path="/checkout" component={Checkout} /> 
+      <Route path="/orders" component={Orders} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/" component={BurgerBuilder} /> 
+      <Redirect to="/" />
+    </Switch>
+    );
+  }
+  console.log(authenticated);
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} /> 
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/login" component={Login} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" component={BurgerBuilder} /> 
-        </Switch>
+        {routes}
       </Layout>
     </div>
   );
